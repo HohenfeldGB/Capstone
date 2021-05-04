d2989010ce250a6e86e3e1ebb35c624903421197
Guilherme Hohenfeld <guilhermehohenfeld@Guilhermes-MacBook-Pro.local>
2021-05-04 10:49:31 -0500

    added function to change voices (didn't make a final decision on the voices yet tho. Just set a male and a female voice)

--- alex.py
+++ alex.py
@@ -7,6 +7,18 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
     
+def getVoices(voice):
+    voices = engine.getProperty('voices')
+    #print(voices[0].id)
+    if voice == 1:
+        engine.setProperty('voice', voices[0].id)
+    if voice == 2:
+        engine.setProperty('voice', voices[4].id)
+
+    speak ("Hello this is alex")
+
 while True:
-    audio = input("Enter the text to convert to speech\n")
-    speak(audio)
+    voice = int(input("Press 1 for male and 2 for female voice\n"))
+#    speak(audio)
+
+    getVoices(voice)

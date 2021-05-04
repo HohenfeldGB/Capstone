f89fb4868745c8b45156fd7df23220c0b2f7e989
Guilherme Hohenfeld <guilhermehohenfeld@Guilhermes-MacBook-Pro.local>
2021-05-04 11:26:43 -0500

    created maps file (to keep track of all dictionaries we need to access) and finished date and time functions

--- alex.py
+++ alex.py
@@ -1,9 +1,12 @@
 import pyttsx3
+import datetime
+import maps
 
 engine = pyttsx3.init() 
 
 def speak(audio):
-    
+    engine.setProperty("rate", 220)
+    engine.setProperty("voice", 4)
     engine.say(audio)
     engine.runAndWait()
     
@@ -17,8 +20,19 @@ def getVoices(voice):
 
     speak ("Hello this is alex")
 
-while True:
-    voice = int(input("Press 1 for male and 2 for female voice\n"))
+
+def time():
+    Time = datetime.datetime.now().strftime("%I:%M") # Hours == I; Minutes == M; Second == S
+    speak("Right now is {}".format(Time))
+    
+def date():
+    year = int(datetime.datetime.now().year)
+    month = int(datetime.datetime.now().month )
+    date = str(datetime.datetime.now().day )
+    speak("Today is {}, {}".format(maps.months[month], date))
+    
+#while True:
+#voice = int(input("Press 1 for male and 2 for female voice\n"))
 #    speak(audio)
 
-    getVoices(voice)
+#getVoices(voice)
